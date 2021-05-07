b53ac74960a464f94fd87bb370227a01be530017
Neevash Ramdial <mail@neevash.dev>
2021-05-06 21:32:48 -0400

    move client initialization to onTap

--- lib/main.dart
+++ lib/main.dart
@@ -4,31 +4,19 @@ import 'package:flutter/material.dart';
 import 'package:stream_feed/stream_feed.dart';
 
 import 'home.dart';
+import 'utils/utils.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-
-  final _client = StreamClient.connect("ay57s8swfnan",
-      token: Token(
-          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoibmVldmFzaC1yYW1kaWFsIn0.yKqSehu_O5WJGh3-aa5qipnBRs7Qtue-1T9TZhT2ejw"));
-
-  runApp(MyApp(client: _client));
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key, required this.client}) : super(key: key);
-
-  final StreamClient client;
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Stream Feed Demo',
       home: LoginScreen(),
-      builder: (context, child) => ClientProvider(
-        client: client,
-        child: child!,
-      ),
     );
   }
 }
@@ -42,9 +30,6 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-
-    /// Access Stream's Client via a provider.
-    final _client = context.client;
 
     return Scaffold(
       body: Padding(
@@ -74,14 +59,19 @@ class _LoginScreenState extends State<LoginScreen> {
                       /// consisting of the user's first, last and full name.
                       /// See class [DummyAppUser] for more details.
                       print("ADDING USER ${user.name}");
-                      final _user =
-                          await _client.setUser(user.data!);
+                      final _client = StreamClient.connect("ay57s8swfnan",
+                          token: user.token);
+
+                      final _user = await _client.setUser(user.data!);
 
                       context.showSnackbar('User Loaded');
 
                       Navigator.of(context).pushReplacement(
                         MaterialPageRoute(
-                          builder: (_) => HomeScreen(streamUser: _user),
+                          builder: (_) => ClientProvider(
+                            client: _client,
+                            child: HomeScreen(streamUser: _user),
+                          ),
                         ),
                       );
                     },
