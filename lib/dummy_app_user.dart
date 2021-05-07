6a74c558d0028fa5447ff8bb95c09b9506e7264e
Neevash Ramdial <mail@neevash.dev>
2021-05-06 21:42:13 -0400

    add demo user tokens

--- lib/dummy_app_user.dart
+++ lib/dummy_app_user.dart
@@ -1,3 +1,5 @@
+import 'package:stream_feed/stream_feed.dart';
+
 enum DummyAppUser {
   sahil,
   sacha,
@@ -37,5 +39,15 @@ extension DummyAppUserX on DummyAppUser {
           'last_name': 'Ramdial',
           'full_name': 'Neevash Ramdial',
         },
+      }[this];
+
+  /// Convenient method Extension to generate a [token] from [DummyAppUser] enum
+  Token? get token => <DummyAppUser, Token>{
+        DummyAppUser.sahil: Token(
+            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoic2FoaWwta3VtYXIifQ.d6RW5eZedEl949w-IeZ40Ukji3yXfFnMw3baLsow028'),
+        DummyAppUser.sacha: Token(
+            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoic2FjaGEtYXJib25lbCJ9.fzDKEyiQ40J4YYgtZxpeQhn6ajX-GEnKZOOmcb-xa7M'),
+        DummyAppUser.nash: Token(
+            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoibmVldmFzaC1yYW1kaWFsIn0.yKqSehu_O5WJGh3-aa5qipnBRs7Qtue-1T9TZhT2ejw'),
       }[this];
 }
