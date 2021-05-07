3caf2ebc82ea232553085e3268fb6d992d615924
Neevash Ramdial <mail@neevash.dev>
2021-05-06 21:42:13 -0400

    add demo user tokens

--- lib/dummy_app_user.dart
+++ lib/dummy_app_user.dart
@@ -1,3 +1,5 @@
+import 'package:stream_feed/stream_feed.dart';
+
 enum DummyAppUser {
   sahil,
   sacha,
@@ -8,9 +10,9 @@ enum DummyAppUser {
 extension DummyAppUserX on DummyAppUser {
   /// Convenient method Extension to generate an [id] from [DummyAppUser] enum
   String? get id => {
-        DummyAppUser.sahil: 'user:sahil-kumar',
-        DummyAppUser.sacha: 'user:sacha-arbonel',
-        DummyAppUser.nash: 'user:neevash-ramdial',
+        DummyAppUser.sahil: 'sahil-kumar',
+        DummyAppUser.sacha: 'sacha-arbonel',
+        DummyAppUser.nash: 'neevash-ramdial',
       }[this];
 
   /// Convenient method Extension to generate a [name] from [DummyAppUser] enum
@@ -37,5 +39,15 @@ extension DummyAppUserX on DummyAppUser {
           'last_name': 'Ramdial',
           'full_name': 'Neevash Ramdial',
         },
+      }[this];
+
+  /// Convenient method Extension to generate a [token] from [DummyAppUser] enum
+  Token? get token => <DummyAppUser, Token>{
+        DummyAppUser.sahil: Token(
+            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoic2FoaWwta3VtYXIifQ.d6RW5eZedEl949w-IeZ40Ukji3yXfFnMw3baLsow028'),
+        DummyAppUser.sacha: Token(
+            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoic2FjaGEtYXJib25lbCJ9.fzDKEyiQ40J4YYgtZxpeQhn6ajX-GEnKZOOmcb-xa7M'),
+        DummyAppUser.nash: Token(
+            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoibmVldmFzaC1yYW1kaWFsIn0.yKqSehu_O5WJGh3-aa5qipnBRs7Qtue-1T9TZhT2ejw'),
       }[this];
 }
